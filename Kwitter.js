a25d5ab9923a1c00b18f80a6af7f550f3e7002ff
Arnav-soni <73822785+Arnav-soni@users.noreply.github.com>
2021-03-18 11:05:08 +0530

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,1 +1,5 @@
-d
+function login(){
+    username = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", username);
+    window.location="kwitter_room.html";
+}
--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,11 @@
-c
+var firebaseConfig = {
+    apiKey: "AIzaSyCMBo5C72KQkkXK5WdMzRZwXmU0xkT818o",
+    authDomain: "mwitter-f7f43.firebaseapp.com",
+    databaseURL: "https://mwitter-f7f43-default-rtdb.firebaseio.com",
+    projectId: "mwitter-f7f43",
+    storageBucket: "mwitter-f7f43.appspot.com",
+    messagingSenderId: "869066985443",
+    appId: "1:869066985443:web:f3fedae9b45b82f2695ad6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig)
